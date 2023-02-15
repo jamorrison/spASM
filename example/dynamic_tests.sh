b5e55ea276cd6a9526fb66190a49e7f1fd2b64aa
Jacob Morrison <Jacob.Morrison@vai.org>
2023-02-15 14:04:39 -0500

    update examples and add genome for the example epibed

--- example/dynamic_tests.sh
+++ example/dynamic_tests.sh
@@ -1,6 +1,6 @@
 #-------------------------------------------------------------------------------
 #
-# Dynamic testing of the GVSU CIS 611 end of semester project
+# Examples of how to run spASM
 #
 # Note, this testing script only works on UNIX systems. It's possible it could
 # be made to work on Windows, but effort would need to be made to ensure this.
@@ -15,45 +15,47 @@
 #
 #    Nov 2022
 #        - Initial creation
+#    Feb 2023
+#        - Update to release examples
 #
 #-------------------------------------------------------------------------------
 
 # Interact with via command line interface
 echo -e "Testing command line interface works"
-echo -e "\t../target/release/cis_611_project test.epibed.gz"
-../target/release/cis_611_project test.epibed.gz
+echo -e "\t../target/release/spasm example.fa.gz test.epibed.gz"
+../target/release/spasm example.fa.gz test.epibed.gz
 
 # Choose a region to focus on
 echo -e "Testing a specific region can be chosen (whole chromosome)"
-echo -e "\t../target/release/cis_611_project -r \"chr1\" test.epibed.gz"
-../target/release/cis_611_project --region "chr1" test.epibed.gz
+echo -e "\t../target/release/spasm -r chr1 example.fa.gz test.epibed.gz"
+../target/release/spasm --region chr1 example.fa.gz test.epibed.gz
 
 echo -e "Testing a specific region can be chosen (chromosome and region)"
-echo -e "\t../target/release/cis_611_project -r \"chr1:0-100\" test.epibed.gz"
-../target/release/cis_611_project --region "chr1:0-100" test.epibed.gz
+echo -e "\t../target/release/spasm -r chr1:0-100 example.fa.gz test.epibed.gz"
+../target/release/spasm --region chr1:0-100 example.fa.gz test.epibed.gz
 
 echo -e "Testing user can combine reads into individual DNA fragments"
-echo -e "\t../target/release/cis_611_project --fragment test.epibed.gz"
-../target/release/cis_611_project --fragment test.epibed.gz
+echo -e "\t../target/release/spasm --fragment example.fa.gz test.epibed.gz"
+../target/release/spasm --merge-mates example.fa.gz test.epibed.gz
 
 echo -e "Testing user can output biscuit ASM file format"
-echo -e "\t../target/release/cis_611_project --biscuit test.epibed.gz"
-../target/release/cis_611_project --biscuit test.epibed.gz
+echo -e "\t../target/release/spasm --biscuit example.fa.gz test.epibed.gz"
+../target/release/spasm --biscuit example.fa.gz test.epibed.gz
 
 echo -e "Testing various false discovery correction implementations"
-echo -e "\t../target/release/cis_611_project --fdr BH test.epibed.gz"
-../target/release/cis_611_project --fdr BH test.epibed.gz
-echo -e "\t../target/release/cis_611_project --fdr BY test.epibed.gz"
-../target/release/cis_611_project --fdr BY test.epibed.gz
-echo -e "\t../target/release/cis_611_project --fdr Bonferroni test.epibed.gz"
-../target/release/cis_611_project --fdr Bonferroni test.epibed.gz
-echo -e "\t../target/release/cis_611_project --fdr Holm test.epibed.gz"
-../target/release/cis_611_project --fdr Holm test.epibed.gz
-echo -e "\t../target/release/cis_611_project --fdr Hochberg test.epibed.gz"
-../target/release/cis_611_project --fdr Hochberg test.epibed.gz
-echo -e "\t../target/release/cis_611_project --fdr None test.epibed.gz"
-../target/release/cis_611_project --fdr None test.epibed.gz
+echo -e "\t../target/release/spasm --fdr BH example.fa.gz test.epibed.gz"
+../target/release/spasm --fdr BH example.fa.gz test.epibed.gz
+echo -e "\t../target/release/spasm --fdr BY example.fa.gz test.epibed.gz"
+../target/release/spasm --fdr BY example.fa.gz test.epibed.gz
+echo -e "\t../target/release/spasm --fdr Bonferroni example.fa.gz test.epibed.gz"
+../target/release/spasm --fdr Bonferroni example.fa.gz test.epibed.gz
+echo -e "\t../target/release/spasm --fdr Holm example.fa.gz test.epibed.gz"
+../target/release/spasm --fdr Holm example.fa.gz test.epibed.gz
+echo -e "\t../target/release/spasm --fdr Hochberg example.fa.gz test.epibed.gz"
+../target/release/spasm --fdr Hochberg example.fa.gz test.epibed.gz
+echo -e "\t../target/release/spasm --fdr None example.fa.gz test.epibed.gz"
+../target/release/spasm --fdr None example.fa.gz test.epibed.gz
 
 echo -e "Testing p-value cutoff change"
-echo -e "\t../target/release/cis_611_project --pcutoff 0.4 --fdr None test.epibed.gz"
-../target/release/cis_611_project --pcutoff 0.4 --fdr None test.epibed.gz
+echo -e "\t../target/release/spasm --pcutoff 0.4 --fdr None example.fa.gz test.epibed.gz"
+../target/release/spasm --pcutoff 0.4 --fdr None example.fa.gz test.epibed.gz
