c6eea709b808d543f538d8b6257e6dee07bc3f8d
Jacob Morrison <Jacob.Morrison@vai.org>
2023-02-23 13:45:22 -0500

    add function to create lookup tables for chr names

--- src/ref_genome.rs
+++ src/ref_genome.rs
@@ -1,5 +1,6 @@
 use std::{
     path::Path,
+    collections::HashMap,
 };
 use rust_htslib::faidx;
 
@@ -11,6 +12,27 @@ use rust_htslib::faidx;
 ///     faidx::Reader
 pub fn open_ref<P: AsRef<Path>>(path: P) -> faidx::Reader {
     faidx::Reader::from_path(path).ok().unwrap()
+}
+
+/// Create lookup table and its reverse to translate chromosomes into IDs
+///
+/// Inputs
+///     path: path to reference (must be FAI indexed)
+/// Returns
+///     ( HashMap::<String, u32>, HashMap::<u32, String> )
+pub fn chromosome_lookup_tables<P: AsRef<Path>>(path: P) -> (HashMap::<String, u32>, HashMap::<u32, String>) {
+    let mut k_chr: HashMap::<String, u32> = HashMap::new();
+    let mut k_int: HashMap::<u32, String> = HashMap::new();
+    let genome = open_ref(path);
+
+    let nseq = genome.fetch_n_seqs();
+    for i in 0..nseq {
+        let seq = genome.fetch_i_seq(i as i32).unwrap();
+        k_chr.insert(seq.clone(), i as u32);
+        k_int.insert(i as u32, seq);
+    }
+
+    (k_chr, k_int)
 }
 
 /// Retrieve base from FASTA
